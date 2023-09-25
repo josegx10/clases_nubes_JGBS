60cc75953db55cdd25ba38a80972759dcf380fae
josegx10 <102836641+josegx10@users.noreply.github.com>
2023-09-25 16:35:09 -0700

    Practica 07

---
+++ practica_07/lib/src/bottom_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class BottonNavigatorPage extends StatefulWidget {
+  @override
+  State<BottonNavigatorPage> createState() => _BottonNavigatorPageState();
+}
+
+class _BottonNavigatorPageState extends State<BottonNavigatorPage> {
+  var _currentIndex = 0;
+
+  final tabs = [
+    Center(
+      child: Text('Mapa', style: TextStyle(color: Colors.red, fontSize: 50)),
+    ),
+    Center(
+      child:
+          Text('E-Mail', style: TextStyle(color: Colors.amber, fontSize: 50)),
+    ),
+    Center(
+      child: Text('Alarma', style: TextStyle(color: Colors.blue, fontSize: 50)),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practica 07'),
+        centerTitle: true,
+        backgroundColor: Colors.green[300],
+      ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.green[200],
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Mapa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            label: 'E-mail',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.alarm),
+            label: 'Alarma',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
