b8dbc19a6b269cc52ef4f7a6cb849d58b3b5b8a1
josegx10 <102836641+josegx10@users.noreply.github.com>
2023-10-15 11:43:22 -0700

    Practica 12

---
+++ practica_12/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:practica_12/src/view_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: ViewPage(),
+    );
+  }
+}
---
+++ practica_12/lib/src/view_page.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+class ViewPage extends StatefulWidget {
+  @override
+  State<ViewPage> createState() => _ViewPageState();
+}
+
+class _ViewPageState extends State<ViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practica 12 - Leer Json'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: FutureBuilder(
+            future: DefaultAssetBundle.of(context)
+                .loadString('assets/pokedex.json'),
+            initialData: [],
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              var showData = json.decode(snapshot.data.toString());
+
+              return ListView.builder(
+                itemCount: showData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(showData[index]['name']['english']),
+                    subtitle: Text(showData[index]['type'].toString()),
+                  );
+                },
+              );
+            }),
+      ),
+    );
+  }
+}
