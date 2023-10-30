58571a7b9b0c4a43f77160ae870d21b8e67b518f
josegx10 <102836641+josegx10@users.noreply.github.com>
2023-10-29 19:53:22 -0700

    Practica 15

---
+++ practica_15/lib/src/slivers_page.dart
@@ -0,0 +1,52 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class SliverPage extends StatelessWidget {
+  final rnd = new Random();
+
+  final List<Color> colores = [
+    Colors.black,
+    Colors.white,
+    Colors.amber,
+    Colors.brown,
+    Colors.cyan,
+    Colors.green,
+    Colors.grey,
+    Colors.orange,
+    Colors.pink,
+    Colors.purple,
+    Colors.lime
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> items = List.generate(
+        70,
+        (index) => Container(
+              height: 100,
+              color: colores[rnd.nextInt(this.colores.length)],
+              width: double.infinity,
+            ));
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('Practica 15 - Slivers'),
+            centerTitle: true,
+            floating: true,
+            pinned: true,
+            expandedHeight: 200,
+            flexibleSpace: Image.network(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRteSE-bKyCugqqHCqaNtKJpWX3Q8nWkpx0Tw&usqp=CAU',
+              fit: BoxFit.cover,
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(items),
+          ),
+        ],
+      ),
+    );
+  }
+}
