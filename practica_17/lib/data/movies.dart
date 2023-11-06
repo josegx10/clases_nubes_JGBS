02846eded8509e89c5d3d05cbd945f3274af3316
josegx10 <102836641+josegx10@users.noreply.github.com>
2023-11-05 20:38:09 -0700

    Practica 17

---
+++ practica_17/lib/data/movies.dart
@@ -0,0 +1,62 @@
+final List<Map<String, dynamic>> movies = [
+  {
+    "id": 1,
+    "title_name": "Music of the Heart",
+    "Genres": "Drama",
+    "image": "http://dummyimage.com/115x188.png/dddddd/000000"
+  },
+  {
+    "id": 2,
+    "title_name": "No Highway in the Sky",
+    "Genres": "Drama|Thriller",
+    "image": "http://dummyimage.com/156x180.png/ff4444/ffffff"
+  },
+  {
+    "id": 3,
+    "title_name": "Two Friends",
+    "Genres": "Drama",
+    "image": "http://dummyimage.com/213x121.png/ff4444/ffffff"
+  },
+  {
+    "id": 4,
+    "title_name": "Cabiria",
+    "Genres": "Adventure|Drama|War",
+    "image": "http://dummyimage.com/105x108.png/ff4444/ffffff"
+  },
+  {
+    "id": 5,
+    "title_name": "Parked",
+    "Genres": "Drama",
+    "image": "http://dummyimage.com/114x152.png/5fa2dd/ffffff"
+  },
+  {
+    "id": 6,
+    "title_name": "Hollywood Party",
+    "Genres": "Comedy|Musical",
+    "image": "http://dummyimage.com/234x103.png/cc0000/ffffff"
+  },
+  {
+    "id": 7,
+    "title_name": "Beyond the Rocks",
+    "Genres": "Drama|Romance",
+    "image": "http://dummyimage.com/151x101.png/ff4444/ffffff"
+  },
+  {
+    "id": 8,
+    "title_name": "Mr. 3000",
+    "Genres": "Comedy|Drama",
+    "image": "http://dummyimage.com/136x105.png/dddddd/000000"
+  },
+  {
+    "id": 9,
+    "title_name": "Kiss Before Dying, A",
+    "Genres": "Film-Noir|Mystery",
+    "image": "http://dummyimage.com/190x147.png/ff4444/ffffff"
+  },
+  {
+    "id": 10,
+    "title_name": "Century",
+    "Genres": "Drama",
+    "image": "http://dummyimage.com/210x210.png/ff4444/ffffff"
+  }
+];
